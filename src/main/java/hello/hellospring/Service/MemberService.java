d91f3119235fd9d0f2bf0dd93d9aad59d177e088
koreaioi <koreaioi@gachon.ac.kr>
2024-03-23 00:37:06 +0900

    회원 서비스 개발

--- src/main/java/hello/hellospring/Service/MemberService.java
+++ src/main/java/hello/hellospring/Service/MemberService.java
@@ -35,7 +35,7 @@ public class MemberService {
 
     private void validateDuplicate(Member member) {
         memberRepository.findByName(member.getName()).ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.")
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
     }
 
