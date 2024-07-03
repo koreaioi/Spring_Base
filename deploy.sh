139363ea75a5cc79059dd084b04f793d84b6c49c
koreaioi <koreaioi@gachon.ac.kr>
2024-07-04 01:44:46 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,16 @@ APP_NAME=action_codedeploy
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f ${APP_NAME}.*.jar)
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 종료할것 없음."
+echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+if [ -z "$CURRENT_PID" ]; then
+	echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+	echo "> kill -15 $CURRENT_PID"
+	kill -15 $CURRENT_PID
+	sleep 5
 fi
 
 echo "> $JAR_PATH 배포"
