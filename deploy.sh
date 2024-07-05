2b762b965a6ba4b0ac70ae284e8616a5b2b7fd85
koreaioi <koreaioi@gachon.ac.kr>
2024-07-05 17:42:45 +0900

    RDS 연결을 위한 의존성 추가

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@
 REPOSITORY=/opt/testapp
 cd $REPOSITORY
 
+# 어플리케이션 이름으로 해야한다 제발!
 APP_NAME=hello-spring
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
+# 현재 어플리케이션 이름으로 실행중인 프로세스 아이디를 찾아서 kill한다.
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f ${APP_NAME}.*.jar)
 
@@ -19,5 +21,6 @@ else
 	sleep 5
 fi
 
+# 그후 재가동
 echo "> $JAR_PATH 배포"
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
